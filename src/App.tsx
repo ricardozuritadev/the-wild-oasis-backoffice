64d82636d88fd505d7695f799d725e26c5d37c50
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2024-08-22 08:53:15 +0200

    Install styled components library

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+    font-size: 1.6rem;
+    margin-bottom: 1.6rem;
+`;
+
 export default function App() {
-    return <div>APP</div>;
+    return (
+        <div>
+            <H1>Styled Components</H1>
+        </div>
+    );
 }
