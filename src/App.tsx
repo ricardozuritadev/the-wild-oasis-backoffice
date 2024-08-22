a2fccc3a3c94dcd362bc7801dfd0209ab8ea6dbe
Ricardo Zurita <ricardoz@clickandgo.com>
2024-08-22 13:43:45 +0200

    Create Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import styled from "styled-components";
+import GlobalStyles from "styles/GlobalStyles";
+import Button from "ui/Button";
+import Heading from "ui/Heading";
 
 const StyledApp = styled.div`
     background-color: rebeccapurple;
 `;
 
 export default function App() {
-    return <StyledApp>App</StyledApp>;
+    return (
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <Heading as="h1">Título</Heading>
+                <Button>Botón</Button>
+            </StyledApp>
+        </>
+    );
 }
