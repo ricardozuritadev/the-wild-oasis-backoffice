978f01df61a70a7bf2538f11cd1e11dc25e622d0
Ricardo Zurita <ricardoz@clickandgo.com>
2024-08-22 09:52:24 +0200

    Create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import styled from "styled-components";
 
-const H1 = styled.h1`
-    font-size: 1.6rem;
-    margin-bottom: 1.6rem;
+const StyledApp = styled.div`
+    background-color: rebeccapurple;
 `;
 
 export default function App() {
-    return (
-        <div>
-            <H1>Styled Components</H1>
-        </div>
-    );
+    return <StyledApp>App</StyledApp>;
 }
